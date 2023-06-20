b0b21979075f3ef607cf8e93f13a54373279b667
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-06-21 00:16:15 +0200

    Update installdepotdownloader64.sh

--- installdepotdownloader64.sh
+++ installdepotdownloader64.sh
@@ -2,9 +2,9 @@
 # Only use this if you're in PRoot.
 apt update && apt full-upgrade -y
 apt install wget libicu72 -y
-wget "https://download.visualstudio.microsoft.com/download/pr/e7866e12-a380-4994-9c56-1bd3a1e0a546/22a5e54cb4e637c5aac7ec6dcab0d739/dotnet-runtime-6.0.16-linux-arm64.tar.gz"
+wget "https://download.visualstudio.microsoft.com/download/pr/29fb12f4-03c2-450c-99d4-6c94fb47a6b2/18e308e40e503f02696f00b719ce581a/dotnet-runtime-6.0.18-linux-arm64.tar.gz"
 mkdir dotnet && cd dotnet
-tar -xf ../dotnet-runtime-6.0.16-linux-arm64.tar.gz
+tar -xf ../dotnet-runtime-6.0.18-linux-arm64.tar.gz
 cd ..
 chmod u+x dotnet/dotnet
 wget "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/depotdownloader-2.5.0.zip"
